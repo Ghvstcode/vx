cd764dd6c909b91035e2acfc2f9d2a89bd5afbc2
Ghvst Code <impoissants@gmail.com>
2021-01-21 08:27:22 +0100

    Update App.vue

--- src/App.vue
+++ src/App.vue
@@ -1,7 +1,7 @@
 <template>
   <div id="app">
     <img alt="Vue logo" src="./assets/logo.png" />
-    <HelloWorld msg="Welcome to Your Vue.js AUTODELPOYED App" />
+    <HelloWorld msg="Welcome to Your AUTODELPOYED Vue.js  App" />
   </div>
 </template>
 
